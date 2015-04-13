e4cda7243f310effa1c902d53aa630a4b3bbca52
Max Glenister <moglenstar@gmail.com>
2015-04-13 10:23:47 +0100

    Tidied up the code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
-function main(){
-    console.log('loaded');
-    var $li = $('<li />',
-        {
-            'class':'header-nav-item'
-        }
-    );
-    var $anchor = $('<a />',
-        {
-            'class':'header-nav-link',
-            'href':'https://github.com/stars'
-        }
-    );
-    var $star = $('<span class="octicon octicon-star"></span>');
-    $anchor.prepend($star);
-    $li.append($anchor);
-    $('.user-nav > li:first-child').after($li);
-}
+(function () {
+    'use strict';
 
-main();
+    var main = function () {
+        var $li = $('<li />',
+            {
+                'class': 'header-nav-item'
+            }
+            );
+        var $anchor = $('<a />',
+            {
+                'class': 'header-nav-link',
+                'href': '/stars'
+            }
+            );
+        var $star = $('<span class="octicon octicon-star"></span>');
+        $anchor.prepend($star).css('margin-top', '2px');
+        $li.append($anchor);
+        $('.user-nav > li:first-child').after($li);
+    };
+
+    main();
+}());
